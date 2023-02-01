ac0c001bd49a16fc3833102e9212943dddf17a89
Davide Guerrazzi <g.davide2005@gmail.com>
2023-02-01 10:32:15 +0100

    Piccolo refactoring, creato GameScene

--- core/src/com/squadra3/tetris/Tetris.java
+++ core/src/com/squadra3/tetris/Tetris.java
@@ -1,36 +1,19 @@
 package com.squadra3.tetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.squadra3.tetris.scenes.GameScene;
 
 // TODO Implementare una vera e propria scena
 public class Tetris extends ApplicationAdapter {
-
-	Camera camera;
-	Viewport v;
+	GameScene scene;
 
 	@Override
 	public void create () {		// Principalmente usato per caricare gli asset
-
-		camera = new OrthographicCamera();
-		
-		v = new FitViewport(400, 800, camera);
+		scene = new GameScene();
 	}
 
 	@Override
 	public void render () {
-		camera.update();
-
-		Gdx.gl.glClearColor(0.05f, 0.05f, 0.05f, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	}
-	
-	@Override
-	public void dispose () {
+		scene.render();
 	}
 }
