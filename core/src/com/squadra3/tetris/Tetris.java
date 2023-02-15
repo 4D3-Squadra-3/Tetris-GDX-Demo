984035b31af281b9bda9e38662c130b69f174140
Davide Guerrazzi <g.davide2005@gmail.com>
2023-02-15 23:46:08 +0100

    Implementata funziolaità base menu principale

--- core/src/com/squadra3/tetris/Tetris.java
+++ core/src/com/squadra3/tetris/Tetris.java
@@ -2,16 +2,19 @@ package com.squadra3.tetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.squadra3.tetris.scenes.GameScene;
+import com.squadra3.tetris.scenes.MainMenu;
 import com.squadra3.tetris.scenes.system.SceneSystem;
 
 public class Tetris extends ApplicationAdapter {
 	@Override
 	public void create () {		// Carico le scene di gioco
+		SceneSystem.addScene(new MainMenu());
 		SceneSystem.addScene(new GameScene());
 	}
 
 	@Override
 	public void render () {
 		SceneSystem.getCurrentScene().render();
+		System.out.println(SceneSystem.getCurrentScene());
 	}
 }
