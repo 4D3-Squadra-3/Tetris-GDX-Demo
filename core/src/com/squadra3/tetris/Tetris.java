2aff5222e0762889f93abc34ca8a5b37e51154f3
Dave <49415276+crazy-catzzz@users.noreply.github.com>
2023-02-16 08:29:19 +0100

    Menu Principale (#7)
    Co-authored-by: Lancel0tt <114827236+Lancel0tt@users.noreply.github.com>

--- core/src/com/squadra3/tetris/Tetris.java
+++ core/src/com/squadra3/tetris/Tetris.java
@@ -2,11 +2,13 @@ package com.squadra3.tetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.squadra3.tetris.scenes.GameScene;
+import com.squadra3.tetris.scenes.MainMenu;
 import com.squadra3.tetris.scenes.system.SceneSystem;
 
 public class Tetris extends ApplicationAdapter {
 	@Override
 	public void create () {		// Carico le scene di gioco
+		SceneSystem.addScene(new MainMenu());
 		SceneSystem.addScene(new GameScene());
 	}
 
