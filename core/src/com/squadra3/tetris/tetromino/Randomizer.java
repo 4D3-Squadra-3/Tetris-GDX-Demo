708e7f35d9ac74f349d5623828113bc9c483297d
Davide Guerrazzi <g.davide2005@gmail.com>
2023-02-23 15:16:54 +0100

    Mini refactoring del Randomizer

--- core/src/com/squadra3/tetris/tetromino/Randomizer.java
+++ core/src/com/squadra3/tetris/tetromino/Randomizer.java
@@ -3,15 +3,7 @@ package com.squadra3.tetris.tetromino;
 import java.util.Random;
 
 public class Randomizer {
-    Shape[] shapes = {
-        Shape.OPIECE,
-        Shape.IPIECE,
-        Shape.JPIECE,
-        Shape.LPIECE,
-        Shape.TPIECE,
-        Shape.SPIECE,
-        Shape.ZPIECE
-    };
+    Shape[] shapes = Shape.values();
 
     public Shape getRandomShape() {
         int value = new Random().nextInt(7);
